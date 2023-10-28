12cd4f32b29787de405ce9c3377fdeedd97c4e98
ezgh <karacaezgik@gmail.com>
2023-10-28 16:55:24 +0300

    Refactor views to generic APIviews

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,18 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AreaViewSet, CategoryViewSet, ListingViewSet
-
-
-router = DefaultRouter()
-router.register(r"categories", CategoryViewSet)
-router.register(r"areas", AreaViewSet)
-router.register(r"listings", ListingViewSet)
-router.register(r"listings/:id", ListingViewSet)
-router.register(r"areas/:id", AreaViewSet)
-router.register(r"categories/:id", CategoryViewSet)
-
+from django.urls import path
+from .views import (
+    AreaList,
+    AreaDetail,
+    CategoryList,
+    CategoryDetail,
+    ListingList,
+    ListingDetail,
+)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("categories/", CategoryList.as_view(), name="category-list"),
+    path("categories/<int:pk>/", CategoryDetail.as_view(), name="category-detail"),
+    path("areas/", AreaList.as_view(), name="area-list"),
+    path("areas/<int:pk>/", AreaDetail.as_view(), name="area-detail"),
+    path("listings/", ListingList.as_view(), name="listing-list"),
+    path("listings/<int:pk>/", ListingDetail.as_view(), name="listing-detail"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,54 @@
-from rest_framework import viewsets
+# views.py
+from rest_framework import generics
 from .serializers import ListingSerializer, CategorySerializer, AreaSerializer
 from .models import Listing, Area, Category
 
 
-# Create your views here.
-
-
-class CategoryViewSet(viewsets.ModelViewSet):
+# all categories
+class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class AreaViewSet(viewsets.ModelViewSet):
+# individual category
+class CategoryDetail(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# all areas
+class AreaList(generics.ListCreateAPIView):
     queryset = Area.objects.all()
     serializer_class = AreaSerializer
 
 
-class ListingViewSet(viewsets.ModelViewSet):
+# individual area
+class AreaDetail(generics.RetrieveAPIView):
+    queryset = Area.objects.all()
+    serializer_class = AreaSerializer
+
+
+# all listings
+class ListingList(generics.ListCreateAPIView):
+    serializer_class = ListingSerializer
+
+    def get_queryset(self):
+        queryset = Listing.objects.all()
+        area_id = self.request.query_params.get("areaId")
+        category_id = self.request.query_params.get("categoryId")
+
+        if area_id:
+            queryset = queryset.filter(area_id=area_id)
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+
+        if not area_id and not category_id:
+            return queryset.order_by("-id")
+        else:
+            return queryset
+
+
+# individual listing
+class ListingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
