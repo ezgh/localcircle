fbf0f13375f04692987339d7badc07385363cd24
ezgh <karacaezgik@gmail.com>
2023-11-05 23:17:51 +0300

    Implement authentication verification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
 import MainLayout from "./components/layouts/MainLayout";
+import LoginLayout from "./components/layouts/LoginLayout";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,30 +13,69 @@ import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import ListingDetail from "./pages/ListingDetail";
 import Profile from "./pages/Profile";
-import LoginLayout from "./components/layouts/LoginLayout";
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const accessToken = Cookies.get("accessToken");
+      const data = {
+        token: accessToken,
+      };
+
+      if (data) {
+        try {
+          const response = await fetch(
+            "http://127.0.0.1:8000/auth/jwt/verify/",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(data),
+            }
+          );
+
+          if (response.status === 200) {
+            setIsAuthenticated(true);
+          } else {
+            setIsAuthenticated(false);
+          }
+        } catch (error) {
+          setIsAuthenticated(false);
+        }
+      }
+    };
+
+    checkAuth();
+  }, []);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/listing/:id" element={<ListingDetail />} />
-          <Route path="/profile/:id" element={<Profile />} />
-        </Route>
-        <Route path="/" element={<LoginLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/activate/:uid/:token" element={<Activate />} />
-          <Route path="/password/reset" element={<ForgotPassword />} />
-          <Route
-            path="/password/reset/confirm/:uid/:token"
-            element={<ResetPassword />}
-          />
-        </Route>
-
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      {isAuthenticated ? (
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/listing/:id" element={<ListingDetail />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<LoginLayout />}>
+            <Route index element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/activate/:uid/:token" element={<Activate />} />
+            <Route path="/password/reset" element={<ForgotPassword />} />
+            <Route
+              path="/password/reset/confirm/:uid/:token"
+              element={<ResetPassword />}
+            />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      )}
     </>
   );
 }
